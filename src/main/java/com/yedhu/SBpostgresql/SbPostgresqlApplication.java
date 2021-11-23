a2f38613ad53a71663b4a2dc68e263cb3963b179
yedhubenny <yedhubenny@gmail.com>
2021-11-23 12:30:09 +0530

    First Commit

---
+++ src/main/java/com/yedhu/SBpostgresql/SbPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.yedhu.SBpostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbPostgresqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbPostgresqlApplication.class, args);
+	}
+
+}
